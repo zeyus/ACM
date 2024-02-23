0b2b4020b87d4fe086f1ce7c92c18d7f0ad0eabe
zeyus <zeyus@zeyus.com>
2024-02-23 14:26:50 +0100

    RW coin in hand model.

--- src/assignment2/RWCoins.jl
+++ src/assignment2/RWCoins.jl
@@ -4,43 +4,78 @@ using FillArrays, Distributions
 using StatsFuns
 using Plots, StatsPlots
 using Random
+using LinearAlgebra
+using Turing: ForwardDiff
 
-function noisy_softmax(x, β)
-    exp_x = exp.(β * x)
-    return exp_x ./ sum(exp_x)
+
+function noisy_softmax(x::Vector{Float64}, β::Float64)
+    u = maximum(x)
+    xt = x .- u
+    lmul!(inv(sum(xt)), xt)
+    return xt
 end
+
 
 # Rescorla-Wagner model
-@model function RWCoins(h, ::Type{T} = Int; N::Int) where T
+# h is the result of selected hands (0 or 1)
+# N is the number of trials
+# reward is a matrix of size N x 2, where reward[t, 1] is the reward of hand 1 at trial t
+# (for this experiment, only one will be 1 and the other will be 0)
+# real_p is the real probability of the coin being in hand 1 (default is 0.8)
+@model function RWCoins(h::Union{Missing, Vector{Union{Missing, Int}}}, ::Type{T} = Float64; N::Int, reward::Matrix{Int}) where T
     # Priors
     α ~ Normal(0, 0.3)  # learning rate
-    β ~ Normal(0, 0.5)  # inverse temperature (noise/explore-exploit tradeoff)
+    β ~ LogNormal(0, 0.5)  # inverse temperature (noise/explore-exploit tradeoff)
+    
 
-
-    p₍ₜ₋₁₎ = [0.5, 0.5]
+    v = Vector{Union{Float64}}([0.0, 0.0])
     if ismissing(h)
-        h = Vector{T}(fill(undef, N))
+        h = Vector{Union{Missing, Int}}(missing, N)
     end
-    for i in 1:N
-        p = noisy_softmax(p₍ₜ₋₁₎, β)
-        h[i] = rand(Categorical(p))
-        δ = h[i] - p₍ₜ₋₁₎[h[i]]
-        p₍ₜ₋₁₎[h[i]] += α * δ
-        p₍ₜ₋₁₎[setdiff(1:2, h[i])] -= α * δ
+
+
+    pred_err::Float64 = 0.0
+    p::Vector{Float64} = [0.5, 0.5]
+    for t in 1:N
+        p = softmax(v)
+        logit_p = β * log(p[2] / p[1])
+        h[t] = rand(BernoulliLogit(logit_p))
+        i = h[t] + 1
+        pred_err += reward[t, i] - v[i]
+        v[i] += ForwardDiff.value(α) * pred_err 
     end
+    return h, α, β
 end
-noisy_softmax([0.5, 0.5], 0.0)
 # read simdata/W3_randomnoise.csv into a dataframe
-df = DataFrame(CSV.File("simdata/W3_randomnoise.csv"))
+# df = DataFrame(CSV.File("simdata/W3_randomnoise.csv"))
 
-# only include noise == 0 and rate == 0.8
-df1 = filter(row -> row.noise == 0 && row.rate == 0.8, df)
+# # only include noise == 0 and rate == 0.8
+# df1 = filter(row -> row.noise == 0 && row.rate == 0.8, df)
 
-# convert the dataframe to a vector
-h = Vector{Int}(df1[!, :choice])
-N = length(h)
+# # convert the dataframe to a vector
+# h = Vector{Int}(df1[!, :choice])
+# N = length(h)
 
-# run the model
-model = RWCoins(h; N=N)
-chains = sample(model, NUTS(1_000, 0.99; max_depth=20), MCMCThreads(), 2_000, 2)
+# run the model (sim)
+N = 120
+hand_p = 0.7
+rh = rand(Bernoulli(hand_p), N)
+lh = 1 .- rh
+reward = [lh rh]
+
+
+h, true_α, true_β = RWCoins(missing; N=N, reward=reward)()
+# parameter recovery
+model = RWCoins(h; N=N, reward=reward)
+chains = sample(model, NUTS(1_000, 0.99; max_depth=20), MCMCThreads(), 3_000, 2)
+chain_df = DataFrame(chains)
+print("True α:", true_α, ", True β:", true_β, "\n")
+
+density(chain_df[!, :α], label="Posterior α", xlabel="value", ylabel="Frequency", title="Posterior", color=:purple, fill=(0, 0.3))
+density!(chain_df[!, :β], label="Posterior β", color=:blue, fill=(0, 0.3))
+# add true values
+vline!([true_α], label="True α", color=:purple, linestyle=:dash)
+vline!([true_β], label="True β", color=:blue, linestyle=:dash)
+
+
 
