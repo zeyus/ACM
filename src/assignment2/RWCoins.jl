01cf38846184f8561d302efc3a41da878e0e521b
zeyus <zeyus@zeyus.com>
2024-03-13 10:59:45 +0100

    plotting stuff

--- src/assignment2/RWCoins.jl
+++ src/assignment2/RWCoins.jl
@@ -11,16 +11,7 @@ using HDF5
 using MCMCChains
 using MCMCChainsStorage
 using CategoricalArrays
-using Logging, LoggingExtras, Dates
-
-# set up logging
-const date_format = "yyyy-mm-dd HH:MM:SS"
-
-timestamp_logger(logger) = TransformerLogger(logger) do log
-  merge(log, (; message = "$(Dates.format(now(), date_format)) $(log.message)"))
-end
-
-ConsoleLogger(stdout, Logging.Info) |> timestamp_logger |> global_logger
+include("../LogCommon.jl")
 
 @info "Preparing model..."
 # Rescorla-Wagner model
