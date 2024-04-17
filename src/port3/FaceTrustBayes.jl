987d9b7ef2512937bf226566ec5d3c32366e07da
zeyus <zeyus@zeyus.com>
2024-04-17 15:10:00 +0200

    Updated model, started sim data

--- src/port3/FaceTrustBayes.jl
+++ src/port3/FaceTrustBayes.jl
@@ -55,6 +55,8 @@ include("../LogCommon.jl")
     subject_bias ~ filldist(Normal(0, 1), N)
     bias = logistic.(μbias .+ σbias .* subject_bias)
     
+    log_lik = zeros(N, Ntrials)
+
     cats = ub-lb
 
     β = 2 * cats # e.g 2 * 7 = 14
@@ -64,9 +66,10 @@ include("../LogCommon.jl")
         
         for j in 1:Ntrials
             SecondRating[i, j] ~ BetaBinomial(cats, 1+α[j]+bias[i], 1+(β-α[j])*bias[i])  # e.g 1+5+0.5, 1+9*0.5
+            log_lik[i, j] = logpdf(BetaBinomial(cats, 1+ReverseDiff.value(α[j])+ReverseDiff.value(bias[i]), 1+(β-ReverseDiff.value(α[j]))*ReverseDiff.value(bias[i])), ReverseDiff.value(SecondRating[i, j]))
         end
     end
-    return SecondRating
+    return (SecondRating, log_lik, mean(log_lik), sum(log_lik))
 end
 
 
@@ -91,16 +94,74 @@ end
     W1 = logistic.(subject_bias .+ μbias .+ σbias .* subject_bias)*2
     W2 = 2 .- W1
 
+    log_lik = zeros(N, Ntrials)
+
     β = 14
     cats = ub-lb
     for i in 1:N
         α = W1[i] * FirstRating[i, :] .+ W2[i] .* (GroupRating[i, :])
         for j in 1:Ntrials
             SecondRating[i, j] ~ BetaBinomial(cats, 1+α[j], 1+β-α[j])
+            log_lik[i, j] = logpdf(BetaBinomial(cats, 1+α[j], 1+β-α[j]), SecondRating[i, j])
         end
     end
+    return (SecondRating, log_lik, mean(log_lik), sum(log_lik))
 end
 
+@info "Setting default sampling params..."
+n_samples = 1_000 # cmdstan default
+ntune = 1_000 # cmdstan default
+nchains = 2 # cmdstan default is 1 but let's use 2
+δ = 0.8 # cmdstan default
+max_depth = 10 # cmdstan default
+
+@info "Generating data..."
+
+μbiases = [0.25, 0.5, 0.75]
+
+sim_results = Dict()
+for μbias in μbiases
+    @info "Generating data for μbias=$μbias..."
+    NSubj = 40
+    NTrials = 153
+    FirstRating = zeros(Int, NSubj, NTrials)
+    GroupRating = zeros(Int, NSubj, NTrials)
+    SecondRating = zeros(Int, NSubj, NTrials)
+    @info "Generating ratings data..."
+    subject_biases = zeros(NSubj)
+    for i in 1:NSubj 
+        subject_diff = rand(Normal(0, 0.01))
+        bias = μbias + subject_diff
+        bias = clamp(bias, 0.01, 0.99)
+        subject_biases[i] = bias
+        FirstRating[i, :] = round.(Int, rand(Uniform(0, 7), NTrials))
+        GroupRating[i, :] = round.(Int, rand(Uniform(0, 7), NTrials))
+
+        # second rating
+        α = FirstRating[i, :] .+ GroupRating[i, :]
+        β = 2 * 7
+        # sample from beta distribution
+        for j in 1:NTrials
+            belief = rand(Beta(1+α[j]*bias, 1+(β-α[j])*bias))
+            SecondRating[i, j] = rand(Binomial(7, belief))
+        end
+    end
+    # fit model
+    @info "Running model...simplebayes"
+    modelSB = FaceTrustSimpleBayes(FirstRating, GroupRating, SecondRating, NSubj, NTrials)
+    chainSB = sample(modelSB, NUTS(ntune, δ, max_depth=max_depth; adtype=Turing.AutoReverseDiff(true)), MCMCThreads(), n_samples, nchains, progress=true, verbose=true)
+    @info "Saving results to sim_results dict..."
+    sim_results["simplebayes_μbias_$μbias"] = Dict(
+        "chains" => chainSB,
+        "FirstRating" => FirstRating,
+        "GroupRating" => GroupRating,
+        "SecondRating" => SecondRating,
+        "μbias" => μbias,
+        "model" => modelSB,
+        "subject_biases" => subject_biases,
+        "generated_quantities" => generated_quantities(modelSB, chainSB)
+    )
+end
 
 @info "Loading data..."
 data = CSV.File("./data/Simonsen_clean.csv") |> DataFrame
@@ -122,23 +183,22 @@ for (i, subdf) in enumerate(Tdata)
 end
 # run the model
 @info "Running model...simplebayes"
-model = FaceTrustSimpleBayes(FirstRating, GroupRating, SecondRating, NSubj, NTrials)
+modelSB = FaceTrustSimpleBayes(FirstRating, GroupRating, SecondRating, NSubj, NTrials)
 # chain = sample(model, NUTS(1_500, 0.65; adtype=Turing.AutoForwardDiff()), MCMCThreads(), 2_000, 4, progress=true, verbose=true)
-chain = sample(model, NUTS(1_500, 0.65; adtype=Turing.AutoReverseDiff(true)), MCMCThreads(), 2_000, 4, progress=true, verbose=true)
+chainSB = sample(modelSB, NUTS(1_500, 0.65; adtype=Turing.AutoReverseDiff(true)), MCMCThreads(), 2_000, 4, progress=true, verbose=true)
 
-chain
 
 # save chain
 @info "Saving chain..."
 h5open("chains/fitted_simplebayes_facetrust.h5", "cw") do file
     g = create_group(file, "simplebayes")
-    write(g, chain)
+    write(g, chainSB)
 end
 
 # show fitted vs true values
 @info "Plotting fitted vs true values..."
-μsamples = chain[:μbias][:, 1]
-σsamples = chain[:σbias][:, 1]
+μsamples = chainSB[:μbias][:, 1]
+σsamples = chainSB[:σbias][:, 1]
 
 predicted_ratings = zeros(Int, NSubj, NTrials, 2_000)
 mode_predicted_ratings = zeros(Int, NSubj, NTrials)
@@ -146,7 +206,7 @@ mode_predicted_ratings = zeros(Int, NSubj, NTrials)
 for (idx, (μ, σ)) in enumerate(zip(μsamples, σsamples))
     for i in 1:NSubj
         α = FirstRating[i, :] .+ GroupRating[i, :]
-        bias = logistic.(μ + σ * chain["subject_bias[$i]"][:, 1][idx])
+        bias = logistic.(μ + σ * chainSB["subject_bias[$i]"][:, 1][idx])
         for j in 1:NTrials
             predicted_ratings[i, j, idx] = rand(BetaBinomial(7, 1+α[j]+bias, 1+(β-α[j])*bias))+1
         end
@@ -166,20 +226,20 @@ p2 = plot(error[3, :], label="Error", xlabel="Trial", ylabel="Error", title="Mea
 
 
 
-modelW = FaceTrustWeightedBayes(FirstRating, GroupRating, SecondRating, NSubj, NTrials)
-chainW = sample(modelW, NUTS(1_500, 0.65; adtype=Turing.AutoReverseDiff(true)), MCMCThreads(), 2_000, 4, progress=true, verbose=true)
+modelWB = FaceTrustWeightedBayes(FirstRating, GroupRating, SecondRating, NSubj, NTrials)
+chainWB = sample(modelWB, NUTS(1_500, 0.65; adtype=Turing.AutoReverseDiff(true)), MCMCThreads(), 2_000, 4, progress=true, verbose=true)
 
 # save chain
 @info "Saving chain..."
 h5open("chains/fitted_weightedbayes_facetrust.h5", "cw") do file
     g = create_group(file, "weightedbayes")
-    write(g, chainW)
+    write(g, chainWB)
 end
 
 # show fitted vs true values
 @info "Plotting fitted vs true values..."
-μsamples = chainW[:μbias][:, 1]
-σsamples = chainW[:σbias][:, 1]
+μsamples = chainWB[:μbias][:, 1]
+σsamples = chainWB[:σbias][:, 1]
 
 predicted_ratings = zeros(Int, NSubj, NTrials, 2_000)
 mode_predicted_ratings = zeros(Int, NSubj, NTrials)
@@ -188,7 +248,7 @@ mode_predicted_ratings = zeros(Int, NSubj, NTrials)
 for (idx, (μ, σ)) in enumerate(zip(μsamples, σsamples))
     for i in 1:NSubj
         α = FirstRating[i, :] .+ GroupRating[i, :]
-        W1 = logistic.(chainW["subject_bias[$i]"][:, 1][idx] .+ μ + σ * chainW["subject_bias[$i]"][:, 1][idx])*2
+        W1 = logistic.(chainWB["subject_bias[$i]"][:, 1][idx] .+ μ + σ * chainWB["subject_bias[$i]"][:, 1][idx])*2
         W2 = 2 .- W1
         for j in 1:NTrials
             predicted_ratings[i, j, idx] = rand(BetaBinomial(7, 1+W1*α[j], 1+W2*(β-α[j])))+1
@@ -209,4 +269,4 @@ p4 = plot(error[5, :], label="Error", xlabel="Trial", ylabel="Error", title="Mea
 
 # testing ParetoSmooth pkg
 @info "Testing ParetoSmooth..."
-psis(modelW, chainW)
+psis(modelWB, chainWB)
